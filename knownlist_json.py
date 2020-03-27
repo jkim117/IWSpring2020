0a64f3f0c237ee580c255b14a7be0ecb7c10569d
Jason Kim <jjk7@princeton.edu>
2020-03-27 19:48:41 -0400

    fixed matching and parsing. Created netassay_bmv2.p4

--- knownlist_json.py
+++ knownlist_json.py
@@ -7,29 +7,49 @@ data["p4info"] = "build/calc2.p4.p4info.txt"
 data["bmv2_json"] = "build/calc2.json"
 data["table_entries"] = []
 
-def dictSetUp():
-    partsDict = {
-        "headers.q1_part1.part": 0,
-        "headers.q1_part2.part": 0,
-        "headers.q1_part4.part": 0,
-        "headers.q1_part8.part": 0,
-        "headers.q2_part1.part": 0,
-        "headers.q2_part2.part": 0,
-        "headers.q2_part4.part": 0,
-        "headers.q2_part8.part": 0,
-        "headers.q2_part16.part": 0,
-        "headers.q3_part1.part": 0,
-        "headers.q3_part2.part": 0,
-        "headers.q3_part4.part": 0,
-        "headers.q3_part8.part": 0,
-        "headers.q3_part16.part": 0,
-        "headers.q4_part1.part": 0,
-        "headers.q4_part2.part": 0,
-        "headers.q4_part4.part": 0
-    }
-    return partsDict
-    
+def dictSetUp(partNum):
+    if (partNum == 1):
+        partsDict = {
+            "headers.q1_part1.part": 0,
+            "headers.q1_part2.part": 0,
+            "headers.q1_part4.part": 0,
+            "headers.q1_part8.part": 0,
+            "headers.q1_part16.part": 0,
+            "user_metadata.domain_id": 0
+        }
+        return partsDict
+    elif (partNum == 2):
+        partsDict = {
+            "headers.q2_part1.part": 0,
+            "headers.q2_part2.part": 0,
+            "headers.q2_part4.part": 0,
+            "headers.q2_part8.part": 0,
+            "headers.q2_part16.part": 0,
+            "user_metadata.domain_id": 0
+        }
+        return partsDict
+    elif (partNum == 3):
+        partsDict = {
+            "headers.q3_part1.part": 0,
+            "headers.q3_part2.part": 0,
+            "headers.q3_part4.part": 0,
+            "headers.q3_part8.part": 0,
+            "headers.q3_part16.part": 0,
+            "user_metadata.domain_id": 0
+        }
+        return partsDict
+    elif (partNum == 4):
+        partsDict = {
+            "headers.q4_part1.part": 0,
+            "headers.q4_part2.part": 0,
+            "headers.q4_part4.part": 0,
+            "headers.q4_part8.part": 0,
+            "headers.q4_part16.part": 0
+        }
+        return partsDict
+    return -1
 
+    
 # Outputs a reversed, 5 digit, binary representation
 def toReversedBinary(num):
     num1 = bin(num)[2::] # cut out 0b prefix
@@ -40,7 +60,7 @@ def toReversedBinary(num):
             num1 = '0' + num1
     return num1[::-1]
 
-def addPart1ToDict(part, partsDict):
+def addPart1ToDict(id2, part, partsDict):
     part1Spec = toReversedBinary(len(part))
 
     charIndex = 0
@@ -56,9 +76,13 @@ def addPart1ToDict(part, partsDict):
     if part1Spec[3] == '1':
         partsDict["headers.q1_part8.part"] = part[charIndex:charIndex+8]
         charIndex = charIndex + 8
+    if part1Spec[4] == '1':
+        partsDict["headers.q1_part16.part"] = part[charIndex:charIndex+16]
+        charIndex = charIndex + 16
+    partsDict["user_metadata.domain_id"] = id2
     return partsDict
 
-def addPart2ToDict(part, partsDict):
+def addPart2ToDict(id3, part, partsDict):
     part2Spec = toReversedBinary(len(part))
     charIndex = 0
 
@@ -77,9 +101,10 @@ def addPart2ToDict(part, partsDict):
     if part2Spec[4] == '1':
         partsDict["headers.q2_part16.part"] = part[charIndex:charIndex+16]
         charIndex = charIndex + 16
+    partsDict["user_metadata.domain_id"] = id3
     return partsDict
 
-def addPart3ToDict(part, partsDict):
+def addPart3ToDict(id4, part, partsDict):
     part3Spec = toReversedBinary(len(part))
     charIndex = 0
 
@@ -98,6 +123,7 @@ def addPart3ToDict(part, partsDict):
     if part3Spec[4] == '1':
         partsDict["headers.q3_part16.part"] = part[charIndex:charIndex+16]
         charIndex = charIndex + 16
+    partsDict["user_metadata.domain_id"] = id4
     return partsDict
 
 def addPart4ToDict(part, partsDict):
@@ -113,6 +139,12 @@ def addPart4ToDict(part, partsDict):
     if part4Spec[2] == '1':
         partsDict["headers.q4_part4.part"] = part[charIndex:charIndex+4]
         charIndex = charIndex + 4
+    if part4Spec[3] == '1':
+        partsDict["headers.q4_part8.part"] = part[charIndex:charIndex+8]
+        charIndex = charIndex + 8
+    if part4Spec[4] == '1':
+        partsDict["headers.q4_part16.part"] = part[charIndex:charIndex+16]
+        charIndex = charIndex + 16
     return partsDict
 
 
@@ -125,67 +157,92 @@ def packageDict(dict_t):
         "action_params": {"id": globalID}
     })
 
+part4Dict = {}
+part3Dict = {}
+part2Dict = {}
+part1Dict = {}
+
 # If len(parts)==1
 def oneparts(parts):
-    if (len(parts[0]) <= 15):
-        dict_t = dictSetUp()
-        addPart1ToDict(parts[0], dict_t)
-        packageDict(dict_t)
-    if (len(parts[0]) <= 31):
-        dict_t = dictSetUp()
-        addPart2ToDict(parts[0], dict_t)
-        packageDict(dict_t)
-        dict_t = dictSetUp()
-        addPart3ToDict(parts[0], dict_t)
-        packageDict(dict_t)
-    if (len(parts[0]) <= 7):
-        dict_t = dictSetUp()
-        addPart4ToDict(parts[0], dict_t)
-        packageDict(dict_t)
+    if parts[0] in part4Dict:
+        return part4Dict[parts[0]]
+    global globalID
+    globalID = globalID + 1
+    part4Dict[parts[0]] = globalID
 
+    dict_t = dictSetUp(4)
+    addPart4ToDict(parts[0], dict_t)
+    
+    data["table_entries"].append({
+        "table": "TopIngress.known_domain_list_q4",
+        "match": dict_t,
+        "action_name": "TopIngress.match_domain4",
+        "action_params": {"id": globalID}
+    })
+    return globalID
 
 # If len(parts)==2
 def twoparts(parts):
-    if (len(parts[0]) <= 15 and len(parts[1]) <= 31):
-        dict_t = dictSetUp()
-        addPart1ToDict(parts[0], dict_t)
-        addPart2ToDict(parts[1], dict_t)
-        packageDict(dict_t)
-    if (len(parts[0]) <= 31 and len(parts[1]) <= 31):
-        dict_t = dictSetUp()
-        addPart2ToDict(parts[0], dict_t)
-        addPart3ToDict(parts[1], dict_t)
-        packageDict(dict_t)
-    if (len(parts[0]) <= 31 and len(parts[1]) <= 7):
-        dict_t = dictSetUp()
-        addPart3ToDict(parts[0], dict_t)
-        addPart4ToDict(parts[1], dict_t)
-        packageDict(dict_t)
+    id4 = oneparts([parts[1]])
+    
+    if parts[0] + str(id4) in part3Dict:
+        return part3Dict[parts[0] + str(id4)]
+    global globalID
+    globalID = globalID + 1
+    part3Dict[parts[0] + str(id4)] = globalID
+
+    dict_t = dictSetUp(3)
+    addPart3ToDict(id4, parts[0], dict_t)
+    
+    data["table_entries"].append({
+        "table": "TopIngress.known_domain_list_q3",
+        "match": dict_t,
+        "action_name": "TopIngress.match_domain3",
+        "action_params": {"id": globalID}
+    })
+    return globalID
 
 # If len(parts)==3
 def threeparts(parts):
-    if (len(parts[0]) <= 15 and len(parts[1]) <= 31 and len(parts[2]) <= 31):
-        dict_t = dictSetUp()
-        addPart1ToDict(parts[0], dict_t)
-        addPart2ToDict(parts[1], dict_t)
-        addPart3ToDict(parts[2], dict_t)
-        packageDict(dict_t)
-    if (len(parts[0]) <= 31 and len(parts[1]) <= 31 and len(parts[2]) <= 7):
-        dict_t = dictSetUp()
-        addPart2ToDict(parts[0], dict_t)
-        addPart3ToDict(parts[1], dict_t)
-        addPart4ToDict(parts[2], dict_t)
-        packageDict(dict_t)
+    id3 = twoparts([parts[1], parts[2]])
+    
+    if parts[0] + str(id3) in part2Dict:
+        return part2Dict[parts[0] + str(id3)]
+    global globalID
+    globalID = globalID + 1
+    part2Dict[parts[0] + str(id3)] = globalID
+
+    dict_t = dictSetUp(2)
+    addPart2ToDict(id3, parts[0], dict_t)
+    
+    data["table_entries"].append({
+        "table": "TopIngress.known_domain_list_q2",
+        "match": dict_t,
+        "action_name": "TopIngress.match_domain2",
+        "action_params": {"id": globalID}
+    })
+    return globalID
 
 # If len(parts)==4
 def fourparts(parts):
-    if (len(parts[0]) <= 15 and len(parts[1]) <= 31 and len(parts[2]) <= 31 and len(parts[3]) <= 7):
-        dict_t = dictSetUp()
-        addPart1ToDict(parts[0], dict_t)
-        addPart2ToDict(parts[1], dict_t)
-        addPart3ToDict(parts[2], dict_t)
-        addPart3ToDict(parts[3], dict_t)
-        packageDict(dict_t)
+    id2 = threeparts([parts[1], parts[2], parts[3]])
+    
+    if parts[0] + str(id2) in part1Dict:
+        return part1Dict[parts[0] + str(id2)]
+    global globalID
+    globalID = globalID + 1
+    part1Dict[parts[0] + str(id2)] = globalID
+
+    dict_t = dictSetUp(1)
+    addPart1ToDict(id2, parts[0], dict_t)
+    
+    data["table_entries"].append({
+        "table": "TopIngress.known_domain_list_q1",
+        "match": dict_t,
+        "action_name": "TopIngress.match_domain1",
+        "action_params": {"id": globalID}
+    })
+    return globalID
 
 def addDomainToTable(domain):
     parts = domain.split('.')
@@ -206,23 +263,15 @@ knownlist = open('known_domains.txt', 'r')
 domains = knownlist.read().split()
 knownlist.close()
 
+addDomainToTable("com")
+addDomainToTable("net")
+addDomainToTable("gov")
+addDomainToTable("edu")
+addDomainToTable("mil")
+addDomainToTable("org")
+addDomainToTable("princeton.edu")
 for d in domains:
     addDomainToTable(d)
-    globalID = globalID + 1
-
-addDomainToTable("princeton.edu")
-globalID = globalID + 1
-addDomainToTable("com")
-globalID = globalID + 1
-addDomainToTable("gov")
-globalID = globalID + 1
-addDomainToTable("edu")
-globalID = globalID + 1
-addDomainToTable("mil")
-globalID = globalID + 1
-addDomainToTable("org")
-globalID = globalID + 1
-addDomainToTable("")
 
 with open('s1-runtime.json', 'w') as outFile:
     json.dump(data, outFile, indent='\t')
