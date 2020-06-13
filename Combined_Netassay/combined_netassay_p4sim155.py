0c9c911739fa216fa9b9447033d5a7e5d697aa97
Jason Kim <jjk7@princeton.edu>
2020-06-13 14:41:54 -0400

    Add combined netassay python scripts

---
+++ Combined_Netassay/combined_netassay_p4sim155.py
@@ -0,0 +1,375 @@
+from sys import argv
+import dpkt
+import csv
+import socket
+import ipaddress
+
+# Data structure and global variables
+allowed_ips = []
+banned_ips = []
+
+knownlistDict = {} # Key is knowlist domain, values are number of dns, number of packets, number of bytes, number missed dns, estimated packets, estimated bytes
+known_domains = []
+
+netassayTable = {} # Key is concatentation of serever IP/client IP. Values is a tuple of domain name, num packets, num of bytes
+TABLE_SIZE = 2048
+usedHash1 = {}
+usedHash2 = {}
+usedHash3 = {}
+usedHash4 = {}
+
+TLSExtensionTypes = {
+    0: 'server_name',
+    1: 'max_fragment_length',
+    2: 'client_certificate_url',
+    3: 'trusted_ca_keys',
+    4: 'truncated_hmac',
+    5: 'status_request',
+    6: 'user_mapping',
+    7: 'client_authz',
+    8: 'server_authz',
+    9: 'cert_type',
+    10: 'elliptic_curves',
+    11: 'ec_point_formats',
+    12: 'srp',
+    13: 'signature_algorithms',
+    14: 'use_srtp',
+    15: 'heartbeat',
+    35: 'session_tickets',
+    13172: 'next_protocol_negotiation',
+    65281: 'renegotiation_info',
+}
+
+class TLSExtension(object):
+    def __init__(self, ext_number, data):
+        self.data = data
+        self.value = ext_number
+
+    @property
+    def name(self):
+        return TLSExtensionTypes.get(self.value, 'unknown')
+
+def is_subnet_of(a, b):
+    return (b.network_address <= a.network_address and b.broadcast_address >= a.broadcast_address)
+
+def matchDomain(known, domain):
+    knownparts = known.split('.')
+    domainparts = domain.split('.')
+    if len(knownparts) != len(domainparts):
+        return False
+    
+    for i in range(0, len(knownparts)):
+        if (knownparts[i] == '*'):
+            continue
+        if (knownparts[i] != domainparts[i]):
+            return False
+    return True
+
+def parse_client_hello(ip_packet, c_hello, ts):
+
+    # Extract domain name
+    for ext in c_hello.extensions:
+        e = TLSExtension(ext[0], ext[1])
+        if TLSExtensionTypes.get(e.value) == 'server_name':
+            domain = str(e.data[5:])[2:-1]
+            break
+    domain_name = domain.split('.')
+
+    # Parser limitations
+    if (len(domain_name) > 5):
+        return
+    for part in domain_name:
+        if (len(part) > 31):
+            return
+
+    clientIP = socket.inet_ntoa(ip_packet.src)
+    serverIP = socket.inet_ntoa(ip_packet.dst)
+    key = clientIP + serverIP
+
+    for d in known_domains:
+        if (matchDomain(d, domain)):
+            entry = knownlistDict[d]
+            knownlistDict[d][0] = knownlistDict[d][0] + 1
+
+            hash1 = hash(serverIP + str(11) + clientIP) % TABLE_SIZE
+            hash2 = hash(str(5) + serverIP + str(3) + clientIP)% TABLE_SIZE
+            hash3 = hash(str(0) + serverIP + str(1) + clientIP)% TABLE_SIZE
+            hash4 = hash(str(7) + serverIP + str(12) + clientIP)% TABLE_SIZE
+
+            if(not hash1 in usedHash1):
+                usedHash1[hash1] = [ts, key, domain]
+            elif (ts - usedHash1[hash1][0] > 300): # timestamp expires
+                usedHash1[hash1] = [ts, key, domain]
+            elif(usedHash1[hash1][1] == key and usedHash1[hash1][2] == domain): # update timestamp for existing entry
+                usedHash1[hash1] = [ts, key, domain]
+
+            elif(not hash2 in usedHash2):
+                usedHash2[hash2] = [ts, key, domain]
+            elif (ts - usedHash2[hash2][0] > 300): # timestamp expires
+                usedHash2[hash2] = [ts, key, domain]
+            elif(usedHash2[hash2][1] == key and usedHash2[hash2][2] == domain): # update timestamp for existing entry
+                usedHash2[hash2] = [ts, key, domain]
+
+            elif(not hash3 in usedHash3):
+                usedHash3[hash3] = [ts, key, domain]
+            elif (ts - usedHash3[hash3][0] > 300): # timestamp expires
+                usedHash3[hash3] = [ts, key, domain]
+            elif(usedHash3[hash3][1] == key and usedHash3[hash3][2] == domain): # update timestamp for existing entry
+                usedHash3[hash3] = [ts, key, domain]
+
+            elif(not hash4 in usedHash4):
+                usedHash4[hash4] = [ts, key, domain]
+            elif (ts - usedHash4[hash4][0] > 300): # timestamp expires
+                usedHash4[hash4] = [ts, key, domain]
+            elif(usedHash4[hash4][1] == key and usedHash4[hash4][2] == domain): # update timestamp for existing entry
+                usedHash4[hash4] = [ts, key, domain]
+
+            else:
+                knownlistDict[d][3] = knownlistDict[d][3]+1
+                return
+
+            
+            # Entry already exists. Hopefully doesn't occur
+            if key in netassayTable:
+                entry = netassayTable[key]
+                if (entry[0] != d):
+                    netassayTable[key][0] = d
+            else:
+                # Create new entry
+                netassayTable[key] = [d, 0, 0]
+
+            break
+
+def parse_dns_response(ip_packet, ts):
+    # Check if it is in the allowed or banned IP lists
+    clientIP = socket.inet_ntoa(ip_packet.dst)
+    cip_object = ipaddress.ip_network(clientIP)
+    allowed = False
+    for ip in allowed_ips:
+        if is_subnet_of(cip_object, ip):
+            allowed = True
+            break
+
+    if (not allowed):
+        return
+
+    for ip in banned_ips:
+        if is_subnet_of(cip_object, ip):
+            return
+
+
+    dns = dpkt.dns.DNS(ip_packet.data.data)
+    answers = dns.an
+
+    domain = answers[0].name
+    domain_name = domain.split('.')
+
+    # Parser limitations
+    if (len(domain_name) > 5):
+        return
+    for part in domain_name:
+        if (len(part) > 31):
+            return
+
+    for d in known_domains:
+        if (matchDomain(d, domain)):
+            entry = knownlistDict[d]
+            knownlistDict[d][0] = knownlistDict[d][0] + 1
+
+            for rr in answers:
+                if (rr.type == 1): #DNS.A
+                    serverIP = socket.inet_ntoa(rr.rdata)
+
+                    key = clientIP + serverIP
+                    hash1 = hash(serverIP + str(11) + clientIP) % TABLE_SIZE
+                    hash2 = hash(str(5) + serverIP + str(3) + clientIP)% TABLE_SIZE
+                    hash3 = hash(str(0) + serverIP + str(1) + clientIP)% TABLE_SIZE
+                    hash4 = hash(str(7) + serverIP + str(12) + clientIP)% TABLE_SIZE
+                    
+                    if(not hash1 in usedHash1):
+                        usedHash1[hash1] = [ts, key, domain]
+                    elif (ts - usedHash1[hash1][0] > 300): # timestamp expires
+                        usedHash1[hash1] = [ts, key, domain]
+                    elif(usedHash1[hash1][1] == key and usedHash1[hash1][2] == domain): # update timestamp for existing entry
+                        usedHash1[hash1] = [ts, key, domain]
+
+                    elif(not hash2 in usedHash2):
+                        usedHash2[hash2] = [ts, key, domain]
+                    elif (ts - usedHash2[hash2][0] > 300): # timestamp expires
+                        usedHash2[hash2] = [ts, key, domain]
+                    elif(usedHash2[hash2][1] == key and usedHash2[hash2][2] == domain): # update timestamp for existing entry
+                        usedHash2[hash2] = [ts, key, domain]
+
+                    elif(not hash3 in usedHash3):
+                        usedHash3[hash3] = [ts, key, domain]
+                    elif (ts - usedHash3[hash3][0] > 300): # timestamp expires
+                        usedHash3[hash3] = [ts, key, domain]
+                    elif(usedHash3[hash3][1] == key and usedHash3[hash3][2] == domain): # update timestamp for existing entry
+                        usedHash3[hash3] = [ts, key, domain]
+
+                    elif(not hash4 in usedHash4):
+                        usedHash4[hash4] = [ts, key, domain]
+                    elif (ts - usedHash4[hash4][0] > 300): # timestamp expires
+                        usedHash4[hash4] = [ts, key, domain]
+                    elif(usedHash4[hash4][1] == key and usedHash4[hash4][2] == domain): # update timestamp for existing entry
+                        usedHash4[hash4] = [ts, key, domain]
+
+                    else:
+                        knownlistDict[d][3] = knownlistDict[d][3]+1
+                        return
+
+                    
+                    # Entry already exists. Hopefully doesn't occur
+                    if key in netassayTable:
+                        entry = netassayTable[key]
+                        if (entry[0] != d):
+                            netassayTable[key][0] = d
+                    else:
+                        # Create new entry
+                        netassayTable[key] = [d, 0, 0]
+
+                    break
+            break
+        
+
+def parse_other(ip_packet, ts):
+    source = socket.inet_ntoa(ip_packet.src) #client
+    dest = socket.inet_ntoa(ip_packet.dst) #server
+    
+    key = source + dest
+    if key in netassayTable:
+        entry = netassayTable[key]
+        netassayTable[key] = [entry[0], entry[1] + 1, entry[2] + ip_packet.len]
+
+        hash1 = hash(dest + str(11) + source) % TABLE_SIZE
+        hash2 = hash(str(5) + dest + str(3) + source)% TABLE_SIZE
+        hash3 = hash(str(0) + dest + str(1) + source)% TABLE_SIZE
+        hash4 = hash(str(7) + dest + str(12) + source)% TABLE_SIZE
+
+        if hash1 in usedHash1 and usedHash1[hash1][1] == key:
+            usedHash1[hash1][0] = ts
+        elif hash2 in usedHash2 and usedHash2[hash2][1] == key:
+            usedHash2[hash2][0] = ts
+        elif hash3 in usedHash3 and usedHash3[hash3][1] == key:
+            usedHash3[hash3][0] = ts
+        elif hash4 in usedHash4 and usedHash4[hash4][1] == key:
+            usedHash4[hash4][0] = ts
+        else:
+            print("error in hash storage")
+            exit(-1)
+
+    else:
+        key = dest + source
+        if key in netassayTable:
+            entry = netassayTable[key]
+            netassayTable[key] = [entry[0], entry[1] + 1, entry[2] + ip_packet.len]
+
+            hash1 = hash(source + str(11) + dest) % TABLE_SIZE
+            hash2 = hash(str(5) + source + str(3) + dest)% TABLE_SIZE
+            hash3 = hash(str(0) + source + str(1) + dest)% TABLE_SIZE
+            hash4 = hash(str(7) + source + str(12) + dest)% TABLE_SIZE
+
+            if hash1 in usedHash1 and usedHash1[hash1][1] == key:
+                usedHash1[hash1][0] = ts
+            elif hash2 in usedHash2 and usedHash2[hash2][1] == key:
+                usedHash2[hash2][0] = ts
+            elif hash3 in usedHash3 and usedHash3[hash3][1] == key:
+                usedHash3[hash3][0] = ts
+            elif hash4 in usedHash4 and usedHash4[hash4][1] == key:
+                usedHash4[hash4][0] = ts
+            else:
+                print("error in hash storage")
+                exit(-1)
+
+
+# parse the command line argument and open the file specified
+if __name__ == '__main__':
+    if len(argv) != 5:
+        print('usage: python netassay_python3_p4sim.py capture.pcap knownlist.txt allowed_dns_dst.txt banned_dns_dst.txt')
+        exit(-1)
+    
+    # Parse allowed IP and banned IP files
+    allowed_ip_file = open(argv[3], 'r')
+    allowed_ip_list = allowed_ip_file.read().split()
+    allowed_ip_file.close()
+    for ip in allowed_ip_list:
+        allowed_ips.append(ipaddress.ip_network(ip))
+
+    banned_ip_file = open(argv[4], 'r')
+    banned_ip_list = banned_ip_file.read().split()
+    banned_ip_file.close()
+    for ip in banned_ip_list:
+        banned_ips.append(ipaddress.ip_network(ip))
+
+    # Create knownlist
+    knownlist = open(argv[2], 'r')
+    known_domains = knownlist.read().split()
+    knownlist.close()
+
+    for d in known_domains:
+        knownlistDict[d] = [0, 0, 0, 0, 0, 0]
+
+    with open(argv[1], 'rb') as f:
+        try:
+            pcap_obj = dpkt.pcap.Reader(f)
+        except:
+            pcap_obj = dpkt.pcapng.Reader(f)
+
+        for ts, buf in pcap_obj:
+            eth = dpkt.ethernet.Ethernet(buf)
+
+            if (eth.type != 2048): # If not IPV4
+                continue
+            ip = eth.data
+            protocol = ip.p
+
+            # For each packet parse the dns responses
+            try:
+                try:
+                    if (protocol == 17 and ip.data.sport == 53):
+                        parse_dns_response(ip, ts)
+                    elif (protocol == 6):
+                        tcp = ip.data
+                        tls = dpkt.ssl.TLS(tcp.data)
+                        if (len(tls.records) < 1 or tls.type != 22):
+                            parse_other(ip, ts)
+                            continue
+                        handshake = dpkt.ssl.TLSHandshake(tls.records[0].data)
+                        
+                        if (handshake.type != 1):
+                            parse_other(ip, ts)
+                            continue
+                        parse_client_hello(ip, handshake.data, ts)
+                    
+                    else:
+                        parse_other(ip, ts)
+                except:
+                    parse_other(ip, ts)
+            except:
+                continue
+
+
+    for i in netassayTable.values():
+        knownlistDict[i[0]][1] = knownlistDict[i[0]][1] + i[1]
+        knownlistDict[i[0]][2] = knownlistDict[i[0]][2] + i[2]
+
+    for i in knownlistDict.keys():
+        num_packets = knownlistDict[i][1]
+        num_bytes = knownlistDict[i][2]
+        num_missed = knownlistDict[i][3]
+        num_dns = knownlistDict[i][0]
+        if (num_dns > 0 and num_missed < num_dns):
+            knownlistDict[i][4] = num_packets / (1 - (num_missed / num_dns))
+            knownlistDict[i][5] = num_bytes / (1 - (num_missed / num_dns))
+
+
+    with open('sim_results.csv', 'w') as csvfile:
+        w = csv.writer(csvfile)
+        w.writerow(["Domain", "Number of DNS requests", "Missed DNS requests missed", "Number of Packets", "Number of Bytes", "Estimated Packets", "Estimated Bytes"])
+
+        for i in knownlistDict.items():
+            w.writerow([i[0], i[1][0], i[1][3], i[1][1], i[1][2], i[1][4], i[1][5]])
+
+            
+
+
---
+++ Combined_Netassay/combined_netassay_python3.py
@@ -0,0 +1,370 @@
+from sys import argv
+import dpkt
+import csv
+import socket
+import ipaddress
+
+# Data structure and global variables
+TOTAL_DNS_RESPONSE_COUNT = 0
+TOTAL_CLIENT_HELLO_COUNT = 0
+NUMBER_DOMAINS_LARGE_PART = 0
+NUMBER_DOMAINS_LARGE_PART_31 = 0
+
+cnameCountDict = {}
+serverIpPrecedenceDict = {}
+serverIpUsed = {}
+precedenceResultsByPairing = {}
+precedenceResultsByPacket = {}
+numDomainLabels = {}
+allowed_ips = []
+banned_ips = []
+NUM_CLIENTS = 0
+NUM_PACKETS = 0
+
+netassayTable = {} # Key is concatentation of serever IP/client IP. Values is a tuple of domain name, num packets, num of bytes
+netassayTableByDomain = {} # Key is domain name
+
+def is_subnet_of(a, b):
+    return (b.network_address <= a.network_address and
+                    b.broadcast_address >= a.broadcast_address)
+
+TLSExtensionTypes = {
+    0: 'server_name',
+    1: 'max_fragment_length',
+    2: 'client_certificate_url',
+    3: 'trusted_ca_keys',
+    4: 'truncated_hmac',
+    5: 'status_request',
+    6: 'user_mapping',
+    7: 'client_authz',
+    8: 'server_authz',
+    9: 'cert_type',
+    10: 'elliptic_curves',
+    11: 'ec_point_formats',
+    12: 'srp',
+    13: 'signature_algorithms',
+    14: 'use_srtp',
+    15: 'heartbeat',
+    35: 'session_tickets',
+    13172: 'next_protocol_negotiation',
+    65281: 'renegotiation_info',
+}
+
+class TLSExtension(object):
+    def __init__(self, ext_number, data):
+        self.data = data
+        self.value = ext_number
+
+    @property
+    def name(self):
+        return TLSExtensionTypes.get(self.value, 'unknown')
+
+def parse_client_hello(ip_packet, c_hello):
+    # Set up global variabes
+    global TOTAL_CLIENT_HELLO_COUNT
+    global NUMBER_DOMAINS_LARGE_PART
+    global NUMBER_DOMAINS_LARGE_PART_31
+    TOTAL_CLIENT_HELLO_COUNT = TOTAL_CLIENT_HELLO_COUNT + 1
+
+    # Extract domain name
+    for ext in c_hello.extensions:
+        e = TLSExtension(ext[0], ext[1])
+        if TLSExtensionTypes.get(e.value) == 'server_name':
+            domain = str(e.data[5:])[2:-1]
+            break
+    domain_name = domain.split('.')
+
+    num_labels = len(domain_name)
+    if num_labels in numDomainLabels:
+        numDomainLabels[num_labels] = numDomainLabels[num_labels] + 1
+    else:
+        numDomainLabels[num_labels] = 1
+
+    # Counter number of DNS responses by domain name
+    if domain in netassayTableByDomain:
+        netassayTableByDomain[domain][0] = netassayTableByDomain[domain][0] + 1
+    else:
+        netassayTableByDomain[domain] = [1, 0, 0]
+
+    for part in domain_name:
+        if (len(part) > 15):
+            NUMBER_DOMAINS_LARGE_PART = NUMBER_DOMAINS_LARGE_PART + 1
+            break
+    for part in domain_name:
+        if (len(part) > 31):
+            NUMBER_DOMAINS_LARGE_PART_31 = NUMBER_DOMAINS_LARGE_PART_31 + 1
+            break
+
+    clientIP = socket.inet_ntoa(ip_packet.src)
+    serverIP = socket.inet_ntoa(ip_packet.dst)
+    key = clientIP + serverIP
+
+    # Entry already exists. Hopefully doesn't occur
+    if key in netassayTable:
+        entry = netassayTable[key]
+        if (entry[0] != domain):
+            netassayTable[key][0] = domain
+    else:
+        # Create new entry
+        netassayTable[key] = [domain, 0, 0]
+
+def parse_dns_response(ip_packet):
+    # Check if it is in the allowed or banned IP lists
+    clientIP = socket.inet_ntoa(ip_packet.dst)
+    cip_object = ipaddress.ip_network(clientIP)
+    allowed = False
+    for ip in allowed_ips:
+        if is_subnet_of(cip_object, ip):
+            allowed = True
+            break
+
+    if (not allowed):
+        return
+
+    for ip in banned_ips:
+        if is_subnet_of(cip_object, ip):
+            return
+
+    # Set up global variabes
+    global TOTAL_DNS_RESPONSE_COUNT
+    global NUMBER_DOMAINS_LARGE_PART
+    global NUMBER_DOMAINS_LARGE_PART_31
+    TOTAL_DNS_RESPONSE_COUNT = TOTAL_DNS_RESPONSE_COUNT + 1
+
+    dns = dpkt.dns.DNS(ip_packet.data.data)
+    answers = dns.an
+
+    cname_count = 0
+    ipPrecedence = 1
+
+    # Extract domain name
+    domain = answers[0].name
+    domain_name = domain.split('.')
+
+    num_labels = len(domain_name)
+    if num_labels in numDomainLabels:
+        numDomainLabels[num_labels] = numDomainLabels[num_labels] + 1
+    else:
+        numDomainLabels[num_labels] = 1
+
+    # Counter number of DNS responses by domain name
+    if domain in netassayTableByDomain:
+        netassayTableByDomain[domain][0] = netassayTableByDomain[domain][0] + 1
+    else:
+        netassayTableByDomain[domain] = [1, 0, 0]
+
+    for part in domain_name:
+        if (len(part) > 15):
+            NUMBER_DOMAINS_LARGE_PART = NUMBER_DOMAINS_LARGE_PART + 1
+            break
+    for part in domain_name:
+        if (len(part) > 31):
+            NUMBER_DOMAINS_LARGE_PART_31 = NUMBER_DOMAINS_LARGE_PART_31 + 1
+            break
+
+    for rr in answers:
+        if (rr.type == 5): #DNS.CNAME
+            cname_count = cname_count + 1
+        elif (rr.type == 1): #DNS.A
+            serverIP = socket.inet_ntoa(rr.rdata)
+
+            key = clientIP + serverIP
+
+            # Entry already exists. Hopefully doesn't occur
+            if key in netassayTable:
+                entry = netassayTable[key]
+                if (entry[0] != domain):
+                    netassayTable[key][0] = domain
+            else:
+                # Create new entry
+                netassayTable[key] = [domain, 0, 0]
+
+            serverIpPrecedenceDict[clientIP + serverIP] = ipPrecedence
+            serverIpUsed[clientIP + serverIP] = False
+            ipPrecedence = ipPrecedence + 1
+
+    if cname_count in cnameCountDict:
+        cnameCountDict[cname_count] = cnameCountDict[cname_count] + 1
+    else:
+        cnameCountDict[cname_count] = 1
+        
+
+def parse_other(ip_packet):
+    source = socket.inet_ntoa(ip_packet.src) #client
+    dest = socket.inet_ntoa(ip_packet.dst) #server
+    keyIPUsed = source + dest
+
+    
+    key = source + dest
+    if key in netassayTable:
+        entry = netassayTable[key]
+        netassayTable[key] = [entry[0], entry[1] + 1, entry[2] + ip_packet.len]
+    else:
+        key = dest + source
+        if key in netassayTable:
+            entry = netassayTable[key]
+            netassayTable[key] = [entry[0], entry[1] + 1, entry[2] + ip_packet.len]
+
+    if (keyIPUsed in serverIpPrecedenceDict):
+        global NUM_PACKETS
+        NUM_PACKETS = NUM_PACKETS + 1
+
+        ipPrecedence = serverIpPrecedenceDict[keyIPUsed]
+        if (not serverIpUsed[keyIPUsed]):
+            global NUM_CLIENTS
+            NUM_CLIENTS = NUM_CLIENTS + 1
+            serverIpUsed[keyIPUsed] = True
+            if (ipPrecedence in precedenceResultsByPairing):
+                precedenceResultsByPairing[ipPrecedence] = precedenceResultsByPairing[ipPrecedence] + 1
+            else:
+                precedenceResultsByPairing[ipPrecedence] = 1
+        
+        if (ipPrecedence in precedenceResultsByPacket):
+            precedenceResultsByPacket[ipPrecedence] = precedenceResultsByPacket[ipPrecedence] + 1
+        else:
+            precedenceResultsByPacket[ipPrecedence] = 1
+
+def matchDomain(known, domain):
+    knownparts = known.split('.')
+    domainparts = domain.split('.')
+    if len(knownparts) != len(domainparts):
+        return False
+    
+    for i in range(0, len(knownparts)):
+        if (knownparts[i] == '*'):
+            continue
+        if (knownparts[i] != domainparts[i]):
+            return False
+    return True
+
+
+# parse the command line argument and open the file specified
+if __name__ == '__main__':
+    if len(argv) != 5:
+        print('usage: python netassay_python3.py capture.pcap knownlist.txt allowed_dns_dst.txt banned_dns_dst.txt')
+        exit(-1)
+    
+    # Parse allowed IP and banned IP files
+    allowed_ip_file = open(argv[3], 'r')
+    allowed_ip_list = allowed_ip_file.read().split()
+    allowed_ip_file.close()
+    for ip in allowed_ip_list:
+        allowed_ips.append(ipaddress.ip_network(ip))
+
+    banned_ip_file = open(argv[4], 'r')
+    banned_ip_list = banned_ip_file.read().split()
+    banned_ip_file.close()
+    for ip in banned_ip_list:
+        banned_ips.append(ipaddress.ip_network(ip))
+
+    with open(argv[1], 'rb') as f:
+        try:
+            pcap_obj = dpkt.pcap.Reader(f)
+        except:
+            pcap_obj = dpkt.pcapng.Reader(f)
+
+        for ts, buf in pcap_obj:
+            eth = dpkt.ethernet.Ethernet(buf)
+
+            if (eth.type != 2048): # If not IPV4
+                continue
+            ip = eth.data
+            protocol = ip.p
+
+            # For each packet parse the dns responses
+            try:
+                try:
+                    if (protocol == 17 and ip.data.sport == 53):
+                        parse_dns_response(ip)
+                    elif (protocol == 6):
+                        tcp = ip.data
+                        tls = dpkt.ssl.TLS(tcp.data)
+                        if (len(tls.records) < 1 or tls.type != 22):
+                            parse_other(ip)
+                            continue
+                        handshake = dpkt.ssl.TLSHandshake(tls.records[0].data)
+                        
+                        if (handshake.type != 1):
+                            parse_other(ip)
+                            continue
+                        parse_client_hello(ip, handshake.data)
+                    
+                    else:
+                        parse_other(ip)
+                except:
+                    parse_other(ip)
+            except:
+                continue
+
+    # Final Stats report
+    print("Total Number of DNS Response: " + str(TOTAL_DNS_RESPONSE_COUNT))
+    print("Total Number of TLS CLient Hellos: " + str(TOTAL_CLIENT_HELLO_COUNT))
+    for x in cnameCountDict.items():
+        print(str(x[0]) + ' CNAME entries -> ' + str(x[1]) + ' DNS responses')
+    print("*********************************************************\n")
+    print("Number of domain names with a part larger than 15 characters: " + str(NUMBER_DOMAINS_LARGE_PART))
+    print("Number of domain names with a part larger than 31 characters: " + str(NUMBER_DOMAINS_LARGE_PART_31))
+    print("*********************************************************\n")
+
+    print("Total number of individual clients: " + str(NUM_CLIENTS))
+    for x in precedenceResultsByPairing.items():
+        print("Number of clients that used IP address in DNS response of precedence: " + str(x[0]) + ": " + str(x[1]))
+    if (1 in precedenceResultsByPairing):
+        print("Percentage of clients that used the first IP address from the DNS response: " + str(precedenceResultsByPairing[1] / float(NUM_CLIENTS)))
+    else:
+        print("Percentage of clients that used the first IP address from the DNS response: 0")
+    print("*********************************************************\n")
+
+    print("Total number of packets: " + str(NUM_PACKETS))
+    for x in precedenceResultsByPacket.items():
+        print("Number of packets from clients that used IP address in DNS response of precedence: " + str(x[0]) + ": " + str(x[1]))
+    if (1 in precedenceResultsByPacket):
+        print("Percentage of packets that used the first IP address from the DNS response: " + str(precedenceResultsByPacket[1] / float(NUM_PACKETS)))
+    else:
+        print("Percentage of packets that used the first IP address from the DNS response: 0")
+    print("*********************************************************\n")
+
+    print("Number of labels in domains:")
+    for x in numDomainLabels.items():
+        print("Domains with " + str(x[0]) + " labels: " + str(x[1]))
+    print("*********************************************************")
+
+
+    for i in netassayTable.values():
+        netassayTableByDomain[i[0]][1] = netassayTableByDomain[i[0]][1] + i[1]
+        netassayTableByDomain[i[0]][2] = netassayTableByDomain[i[0]][2] + i[2]
+
+
+    with open('allresults.csv', 'w') as csvfile:
+        w = csv.writer(csvfile)
+        w.writerow(["Domain", "Number of DNS requests", "Number of Packets", "Number of Bytes"])
+
+        for i in netassayTableByDomain.items():
+            w.writerow([i[0], i[1][0], i[1][1], i[1][2]])
+
+    # Create knownlist csv if argument provided
+    knownlist = open(argv[2], 'r')
+    domains = knownlist.read().split()
+    knownlist.close()
+
+    knownlistDict = {} # Key is knowlist domain, values are number of dns, number of packets, number of bytes
+    for d in domains:
+        knownlistDict[d] = [0, 0, 0]
+
+    for i in netassayTableByDomain.items():
+        for d in domains:
+            if (matchDomain(d, i[0])):
+                entry = knownlistDict[d]
+                knownlistDict[d] = [entry[0] + i[1][0], entry[1] + i[1][1], entry[2] + i[1][2]]
+                break
+    
+    with open('knownlistresults.csv', 'w') as csvfile:
+        w = csv.writer(csvfile)
+        w.writerow(["Domain", "Number of DNS requests", "Number of Packets", "Number of Bytes"])
+
+        for i in knownlistDict.items():
+            w.writerow([i[0], i[1][0], i[1][1], i[1][2]])
+
+            
+
+
