f8a503f6ba46630b9c8822a6ad1ba121a7af4f1c
Jason Kim <jjk7@princeton.edu>
2020-10-25 16:31:40 -0400

    asdf

--- DNS_Netassay/netassay_python_preprocess.py
+++ DNS_Netassay/netassay_python_preprocess.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
                 continue
             ip = eth.data
             protocol = ip.p
-            packet_len = eth.len
+            packet_len = eth.__len__()
 
             packet_processed = False
             try:
